c5a506021de7c9510b87eea078cb6b060709250c
Gu Zhenglin <73575529+guzhenglin@users.noreply.github.com>
2022-03-15 13:47:32 +0800

    debug

--- buffon_nopi.m
+++ buffon_nopi.m
@@ -1,8 +1,8 @@
-l=0.5;      %针的长度
-a=1;        %平行线间距
-N=1000;     %投针总次数
-n=0;        %针与平行线相交的次数
-%绘制平行线
+l=0.5;      
+a=1;        
+N=1000;     
+n=0;        
+
 x=[0:2];
 for i=[0,1,2]
     y=a*i*ones(1,3);
@@ -10,10 +10,10 @@ for i=[0,1,2]
     hold on;
 end
 axis([-1,3,-1,2*a+1]);
-%模拟投针
+
 for j=1:N
     x1=rand(1)*2;
-    y1=rand(1)*2;   %确定中点坐标
+    y1=rand(1)*2;  
     x0=rand(1)*l-l/2;
     x2=x1+x0;
     y2=y1+sqrt((l/2)*(l/2)-x0*x0);
@@ -22,7 +22,7 @@ for j=1:N
     plot([x2,x3],[y2,y3],'red');
     hold on;
     d=rem(y1,a);
-    if(d<=0.5*a)                 %判断是否相交
+    if(d<=0.5*a)                 
         if(d<sqrt((l/2)*(l/2)-x0*x0))
             n=n+1;
         end
@@ -33,4 +33,4 @@ for j=1:N
         end
     end
 end
-pisimul=4*a*n/(l*N)            %计算pi值
+pisimul=4*a*n/(l*N)            
